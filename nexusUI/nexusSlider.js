3d739820cd7f20cb97366392106c63db460fef64
Ben Taylor <taylorbf@gmail.com>
2013-06-08 17:50:18 -0400

    Updated gain slider

--- nexusUI/nexusSlider.js
+++ nexusUI/nexusSlider.js
@@ -39,7 +39,56 @@ function slider(target, ajaxCommand, oscName, uiIndex, oscIp) {
 	this.draw = function() {
 		self.erase();
 		self.makeRoundedBG();
+		
+		var level = self.value;
+		var x1 = self.padding;
+		var y1 = self.height-self.value*self.height;
+		var x2 = self.padding+self.sliderWidth;
+		var y2 = self.height-self.padding;
+		var depth = self.padding*2;
+		
+		
 		with (this.context) {
+			strokeStyle = self.colors.border;
+			fillStyle = self.colors.fill;
+			lineWidth = self.lineWidth;
+			stroke();
+			fill();
+			
+			strokeStyle = this.colors.accent;
+			fillStyle = this.colors.accent;
+			lineWidth = 5;
+	    	
+			beginPath();
+			if (self.value>0.97) {
+				moveTo(x1+depth, y1); //TOP LEFT
+				lineTo(x2-depth, y1); //TOP RIGHT
+				quadraticCurveTo(x2, y1, x2, y1+depth);
+			} else {
+				moveTo(x1, y1); //TOP LEFT
+				lineTo(x2, y1); //TOP RIGHT
+				//stroke();
+			}
+			lineTo(x2, y2-depth); //BOTTOM RIGHT
+			quadraticCurveTo(x2, y2, x2-depth, y2);
+			lineTo(x1+depth, y2); //BOTTOM LEFT
+			quadraticCurveTo(x1, y2, x1, y2-depth);
+			if (self.value>0.95) {
+				lineTo(x1, y1+depth); //TOP LEFT
+				quadraticCurveTo(x1, y1, x1+depth, y1);
+			} else {
+				lineTo(x1, y1); //TOP LEFT
+			}
+			if (self.value>0.03) {
+			//	globalAlpha = 0.3;
+				fill();	
+			//	globalAlpha = 1;
+			}
+			closePath();
+		} 
+		
+		
+	/*	with (this.context) {
 			strokeStyle = self.colors.border;
 			fillStyle = self.colors.fill;
 			lineWidth = self.lineWidth;
@@ -60,7 +109,7 @@ function slider(target, ajaxCommand, oscName, uiIndex, oscIp) {
 			fill();
 			closePath();
 			globalAlpha = 1;
-		}
+	} */
 	}
 	
 	this.click = function() {
@@ -69,7 +118,7 @@ function slider(target, ajaxCommand, oscName, uiIndex, oscIp) {
 
 	this.move = function() {
 		if (self.clicked) {
-			self.value = (Math.abs((self.clickPos.y / self.height) - 1));
+			self.value = (Math.abs((nx.clip(self.clickPos.y / self.height, 0.01, 0.98)) - 1));
 			self.draw();
 		}
 		self.nxTransmit(self.value);
