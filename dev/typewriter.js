45913369e7a84fac93a59554d119b70b3891152c
Ben Taylor <taylorbf@gmail.com>
2014-06-01 16:41:47 -0500

    Progress on typewriter object
    establishing first two rows of keys and general interaction

--- dev/typewriter.js
+++ dev/typewriter.js
@@ -1,306 +1,144 @@
-// nexusUI - Keyboard
-//
-// nexusKeyboard transmits midi pair arrays of [ note number, on/off ]
-// Middle C "pressed" message will look like [12,1]
-// Middle C "unpressed" message will look like [12,0]
-// If sent to Max, these will show up as two-number lists.
+// nexusUI - Typewriter (computer keyboard)
 
-// FIXME: key detection not accurate when changed num of octaves!
-
-function keyboard(target, transmitCommand) {
+function typewriter(target, transmitCommand) {
 
 	//self awareness
 	var self = this;
-	this.defaultSize = { width: 400, height: 100 };
+	this.defaultSize = { width: 400, height: 150 };
 	
 	//get common attributes and methods
 	getTemplate(self, target, transmitCommand);
 
-	// define unique attributes
-	self.octaves = 2;
-//	var width = (self.canvas.width/(self.octaves*12))/3;
-	var width = (self.canvas.width/(self.octaves*12))/1.75;
-	var w_height = self.height;
-	var b_height = w_height*4/7;
-	var w_width = width*3;
-	var b_width = width*2;
-	// [On/Off, order of white or black, white(0) or black(1), start_position of X, end_position of X]
-	var black_dis = [0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 4, 5];
-	var white_dis = [[0, 2], [4, 5], [7, 9], [9, 11], [13, 14], [16, 17], [19, 21]];
-	var order = [0, 2, 4, 5, 7, 9, 11, 1, 3, 6, 8, 10];
-	var keys = new Array();
-	self.lineWidth = 1;
+	var rowLens = [ 14, 14, 13, 12, 11]
 
-	var note_new;
-	var note_old;
+	this.keywid = self.width/14.5;
+
+	this.rows = [
+		[
+			{ symbol: "`", value: 192, width: 1, on: false },
+			{ symbol: "1", value: 49, width: 1, on: false  },
+			{ symbol: "2", value: 50, width: 1, on: false  },
+			{ symbol: "3", value: 51, width: 1, on: false  },
+			{ symbol: "4", value: 52, width: 1, on: false  },
+			{ symbol: "5", value: 53, width: 1, on: false  },
+			{ symbol: "6", value: 54, width: 1, on: false  },
+			{ symbol: "7", value: 55, width: 1, on: false  },
+			{ symbol: "8", value: 56, width: 1, on: false  },
+			{ symbol: "9", value: 57, width: 1, on: false  },
+			{ symbol: "0", value: 48, width: 1, on: false  },
+			{ symbol: "-", value: 59, width: 1, on: false  },
+			{ symbol: "=", value: 60, width: 1, on: false  },
+			{ symbol: "delete", value: 61, width: 1.5, on: false  }
+		],
+		[
+			{ symbol: "tab", value: 10, width: 1.5, on: false  },
+			{ symbol: "q", value: 10, width: 1, on: false  },
+			{ symbol: "w", value: 10, width: 1, on: false  },
+			{ symbol: "e", value: 10, width: 1, on: false  },
+			{ symbol: "r", value: 10, width: 1, on: false  },
+			{ symbol: "t", value: 10, width: 1, on: false  },
+			{ symbol: "y", value: 10, width: 1, on: false  },
+			{ symbol: "u", value: 10, width: 1, on: false  },
+			{ symbol: "i", value: 10, width: 1, on: false  },
+			{ symbol: "o", value: 10, width: 1, on: false  },
+			{ symbol: "p", value: 10, width: 1, on: false  },
+			{ symbol: "[", value: 10, width: 1, on: false  },
+			{ symbol: "]", value: 10, width: 1, on: false  },
+			{ symbol: "\\", value: 10, width: 1, on: false  }
+		]
+	]
 		
 	this.init = function() {
 		document.addEventListener("keydown", self.type);
 		document.addEventListener("keyup", self.untype);
 		
-		width = (self.canvas.width/(self.octaves*12))/1.75;
-		w_height = self.height;
-		b_height = w_height*4/7;
-		w_width = width*3;
-		b_width = width*2;
-		
-		
-		var o,j,i;
-		for (j=0;j<self.octaves;j++) {
-			for (i=0; i<12; i++) {
-				o = order[i]+j*12;
-				if (i<7) {
-					var u1 = w_width*(i + j*7);
-					var y = i + 1;
-					var u2 = w_width*(y + j*7);
-					keys.push([0, i, 0, u1, u2, o]);
-				}
-				else {
-					var k = black_dis[i];
-					var t1 = b_width*(1 + k + k/2) + 7*j*w_width;
-					var r = k + 1;
-					var t2 = b_width*(1 + r + k/2) + 7*j*w_width;
-					keys.push([0, k, 1, t1, t2, o]);
-				}
-			}
-		}
-		
 		self.draw();
-		
-		return 1;
 	}
 
-	this.draw = function() {
-		var m,i,d,xx, dis;
+	this.draw = function() {	// erase
+		self.erase();
 
-		for(m=0;m<self.octaves;m++) {
-			for (i=0;i<12;i++){
-				d = m*12 + i;
-				if (keys[d][2] == 0) {
-					var k = keys[d][1];
-					var x = k*w_width + (m*w_width*7);
-					with (self.context) {
-						lineWidth = self.lineWidth;
-						if (keys[d][0] == 0){
-							fillStyle = self.colors.fill;
-							fillRect(x, 0, w_width, w_height);
-							strokeStyle = self.colors.border;
-							strokeRect(x , 0, w_width, w_height);
+		//make background path
+		self.makeRoundedBG();
 
-						}
-						else {
-							fillStyle = self.colors.accent;
-							fillRect(x, 0, w_width, w_height);
-						}
-
-					}
-				}
-				else {
-					dis = keys[d][1];
-					xx = dis*(b_width+b_width/2) + b_width + (m*w_width*7);	
-					with (self.context) {
-						lineWidth = self.lineWidth;
-						if (keys[d][0] == 0){
-							fillStyle = self.colors.black;
-						}	
-						else {
-							fillStyle = self.colors.accent;
-						}
-						fillRect(xx, 0, b_width, b_height);	
-					}
-				}	
-			}
-		}
 		with (self.context) {
+			//fill in background path
 			strokeStyle = self.colors.border;
-			lineWidth = 3;
-			strokeRect(0,0,self.width,self.height);
+			fillStyle = self.colors.fill;
+			lineWidth = self.lineWidth;
+			stroke();
+			fill();
+
+			strokeStyle = self.colors.black 
+			fillStyle = self.colors.black 
+			lineWidth = 1
+
+			for (var i=0;i<self.rows.length;i++) {
+				var currkeyL = 0;
+				for (var j=0;j<self.rows[i].length;j++) {
+					if (self.rows[i][j].on) {
+						fillRect(currkeyL,i*30,self.keywid*self.rows[i][j].width,30);
+					} else {
+						strokeRect(currkeyL,i*30,self.keywid*self.rows[i][j].width,30);
+					}
+					currkeyL += self.keywid*self.rows[i][j].width;
+				}
+			}
+
 		}
 		self.drawLabel();
 	}
 
-	this.change_cell = function(whichCell, number) {
-		if(whichCell != null){
-			keys[whichCell].splice(0,1,number);
-		}
-	}
-
-	// "WhichKey_pressed" find out the key, and changes the cell of the array(keys[]) and pass it into variable "note_new"
-	this.whichKey_pressed = function (x, y){
-		var found_click = 0;
-		var j,i,k;
-
-		if (y < b_height){
-			for (j=0; j<self.octaves; j++){
-				for (i=7; i<12; i++) {
-					var d = j*12 + i;
-					if (x > keys[d][3] && x <= keys[d][4]) {
-						note_new = d;
-						found_click = 1;
-						break;
-					}
-				}
-				if (found_click == 0) {
-					for (k=0; k<7; k++) {
-						var sp = (white_dis[k][0]+(21*j))*width;
-						var ep = (white_dis[k][1]+(21*j))*width;
-						if (x > sp && x <= ep) {
-							var o = j*12 + k;
-							note_new = o;
-							break;
-						}					
-					}
-				}
-			}
-
-		}
-		else if (y > b_height && y < w_height) {
-			for (j=0; j<self.octaves; j++){
-				for (i=0; i<7; i++) {
-					var d = j*12 + i;
-					if (x > keys[d][3] && x < keys[d][4]) {
-						note_new = d;
-					}
-				}
-			}
-		}
-		else {
-			note_new = null;
-		}
-	}
 
 	// 
 	this.click = function(e) {
-		self.whichKey_pressed(self.clickPos.x, self.clickPos.y);
-		self.change_cell(note_new, 1);
-		note_old = note_new;
-		
-		midi_note = keys[note_new][5];
-		
-		// change the note_new --> midi_note_new (offset)
-		var note = [midi_note, 1];
-	//	note = note[0] + " " + note[1]
+	
 		self.nxTransmit(note);
-		console.log(note);
 		self.draw();	
 	}
 
 	this.move = function(e) {
 		if (self.clicked) {
-			self.whichKey_pressed(self.clickPos.x,self.clickPos.y);
-			if (note_old != note_new) {
-				self.change_cell(note_old, 0);
-				self.change_cell(note_new, 1);
-				midi_note = keys[note_new][5];
-			//	self.nxTransmit(midi_note+" "+1);
-				self.nxTransmit([midi_note, 1]);
-				midi_note = keys[note_old][5];
-				self.nxTransmit([midi_note, 0]);
-			//	self.nxTransmit(midi_note+" "+0);
-				self.draw();
-			}
+			self.draw();
 		}
-		note_old = note_new;
 	}
 
 	this.release = function(e) {
-		for (j=0;j<self.octaves;j++) {
-			for (i=0;i<12;i++) {
-				var note_released = j*12 + i;
-				self.change_cell(note_released, 0);
-			}
-		}
-		midi_note = keys[note_new][5];
-		self.nxTransmit([midi_note, 0]);
-	//	self.nxTransmit(midi_note+" "+0);
-		self.draw();
-	}
 	
-	this.touch = function(e) {
-		self.whichKey_pressed(self.clickPos.x, self.clickPos.y);
-		self.change_cell(note_new, 1);
-		note_old = note_new;
-		
-		midi_note = keys[note_new][5];
-		
-		// change the note_new --> midi_note_new (offset)
-		self.nxTransmit([midi_note, 1]);
-		//self.nxTransmit(midi_note+" "+1);
-		self.draw();
-	}
-
-	this.touchMove = function(e) {
-		if(self.clicked) {
-		self.clickPos = self.getTouchPosition(e, self.offset);;
-
-		self.whichKey_pressed(this.clickPos.x,this.clickPos.y);
-			if (note_old != note_new) {
-				self.change_cell(note_old, 0);
-				self.change_cell(note_new, 1);
-				midi_note = keys[note_new][5];
-				self.nxTransmit([midi_note, 1]);
-				//self.nxTransmit(midi_note+" "+1);
-				midi_note = keys[note_old][5];
-				self.nxTransmit([midi_note, 0]);
-				//self.nxTransmit(midi_note+" "+0);
-				self.draw();
-			}
-		}
-		note_old = note_new;
-	}
-
-	this.touchRelease = function(e) {
-		for (j=0;j<self.octaves;j++) {
-			for (i=0;i<12;i++) {
-				var d = j*12 + i;
-					self.change_cell(d, 0);
-			}
-		}
-		midi_note = keys[note_new][5];
 		self.nxTransmit([midi_note, 0]);
-		//self.nxTransmit(midi_note+" "+0);
 		self.draw();
 	}
+
 	
 	this.type = function(e) {
 		var currKey = e.which;
-		if (e.which>47 && e.which<91) {
-			var asciis = [81,50,87,51,69,82,53,84,54,89,55,85];
-			var keyIndex = [0,7,1,8,2,3,9,4,10,5,11,6 ];
-			var keyAsciiIndex = asciis.indexOf(currKey);
-			if (keyAsciiIndex!=-1) {
-				note_new = keyIndex[keyAsciiIndex];
-				self.change_cell(note_new, 1);
-				note_old = note_new;
-				
-				midi_note = keys[note_new][5];
-				
-				// change the note_new --> midi_note_new (offset)
-				self.nxTransmit(midi_note);
-			//	self.nxTransmit(midi_note+" "+1);
-				self.draw();	
+		for (var i=0;i<self.rows.length;i++) {
+			for (var j=0;j<self.rows[i].length;j++) {
+				if (currKey == self.rows[i][j].value) {
+					console.log(self.rows[i][j].symbol)
+					self.rows[i][j].on = true;
+					break;
+				}
 			}
 		}
+		//self.nxTransmit();
+		self.draw();	
 	}
 	
 	this.untype = function(e) {
+	
 		var currKey = e.which;
-		if (e.which>47 && e.which<91) {
-			var asciis = [  81,50,87,51,69,82,53,84,54,89,55,85];
-			var keyIndex = [0,7,1,8,2,3,9,4,10,5,11,6 ];
-			var keyAsciiIndex = asciis.indexOf(currKey);
-			if (keyAsciiIndex!=-1) {
-				note_old = keyIndex[keyAsciiIndex];
-				self.change_cell(note_old, 0);
-				
-				midi_note = keys[note_new][5];
-				
-				// change the note_new --> midi_note_new (offset)
-				self.nxTransmit(midi_note);
-			//	self.nxTransmit(midi_note+" "+0);
-				self.draw();
+		for (var i=0;i<self.rows.length;i++) {
+			for (var j=0;j<self.rows[i].length;j++) {
+				if (currKey == self.rows[i][j].value) {
+					console.log(self.rows[i][j].symbol)
+					self.rows[i][j].on = false;
+					break;
+				}
 			}
-		}	
+		}
+		//self.nxTransmit();
+		self.draw();
 	}
 	
 }
