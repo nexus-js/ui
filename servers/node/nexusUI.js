ca2ea800bc779a84f9ef49806172a00346036845
Ben Taylor <taylorbf@gmail.com>
2014-06-15 23:16:19 -0500

    doc property testing

--- servers/node/nexusUI.js
+++ servers/node/nexusUI.js
@@ -103,9 +103,9 @@ function button(target, transmitCommand) {
 	this.value = 1;
 
 	/** @property {object}  val    
-		* *press* &nbsp; 0 (clicked) or 1 (unclicked)<br>
-		* *x* &nbsp; 0-1 float of x-position of click ("node" mode only)<br>
-		* *y* &nbsp; 0-1 float of y-position of click ("node" mode only)<br> 
+		+ *press* &nbsp; 0 (clicked) or 1 (unclicked)<br>
+		+ *x* &nbsp; 0-1 float of x-position of click ("node" mode only)<br>
+		+ *y* &nbsp; 0-1 float of y-position of click ("node" mode only)<br> 
 		*/
 	this.val = {
 		press: 0
