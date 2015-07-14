07eeb8d15acd5d173a41728e0a30d8b3ad36db4c
Ben Taylor <taylorbf@gmail.com>
2015-07-14 16:57:18 -0400

    matrix timing, envelope demo fix, position animation

--- dist/nexusUI.js
+++ dist/nexusUI.js
@@ -51,6 +51,7 @@ var timingUtils = require('../utils/timing');
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 var transmit = require('../utils/transmit');
+var WAAClock = require('waaclock');
 
 
 var manager = module.exports = function() {
@@ -110,10 +111,34 @@ var manager = module.exports = function() {
   this.fontWeight = "bold";
 
   this.context = window.audioContext || new AudioContext();
+  this.clock = new WAAClock(this.context)
+  this.clock.start()
 
+  this.interval = {
+    add: function(func,ms) {
+      var event = nx.clock.setTimeout(func,1).repeat(1)
+      nx.clock.timeStretch(nx.context.currentTime, [event], ms/1000)
+      console.log(event)
+      return event
+    },
+    kill: function(event) {
+      event.clear()
+    },
+    bpm: function(event,bpm) {
+      console.log(bpm + " in int")
+      console.log(event)
+      if (bpm) {
+        nx.clock.timeStretch(nx.context.currentTime, [event], bpm)
+      }
+    },
+    ms: function(event,ms) {
+      nx.clock.timeStretch(nx.context.currentTime, [event], ms/1000)
+    }
+  }
 
   /**  @property {integer} throttlePeriod Throttle time in ms (for nx.throttle). */
   this.throttlePeriod = 20;
+
 }
 
 util.inherits(manager, EventEmitter)
@@ -419,7 +444,7 @@ manager.prototype.blockMove = function(e) {
      e.stopPropagation ? e.stopPropagation() : false;
   }
 }
-},{"../utils/timing":7,"../utils/transmit":8,"../widgets":17,"events":42,"util":46}],3:[function(require,module,exports){
+},{"../utils/timing":7,"../utils/transmit":8,"../widgets":17,"events":42,"util":46,"waaclock":48}],3:[function(require,module,exports){
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 var domUtils = require('../utils/dom');
@@ -2225,6 +2250,8 @@ var envmulti = module.exports = function (target) {
 
 	// Index of which node was clicked
 	var selectedNode = null;
+	
+	nx.aniItems.push(this.pulse.bind(this));
 
 	this.init();
 }
@@ -2234,10 +2261,6 @@ util.inherits(envmulti, widget);
 envmulti.prototype.init = function() {
 	this.mindim = this.width < this.height ? this.width : this.height;
 	this.draw();
-	nx.aniItems.push(this.pulse.bind(this));
-
-	// init nx
-	nx.startPulse();
 }
 
 envmulti.prototype.draw = function() {
@@ -3416,6 +3439,7 @@ var matrix = module.exports = function (target) {
 	this.place = null;
 
 	this.starttime;
+	this.lastbeat;
 	this.thisframe = 0;
 	this.lastframe = 0;
 	this.context.lineWidth = 1;
@@ -3434,6 +3458,10 @@ var matrix = module.exports = function (target) {
 	```
 	*/
 	this.bpm = 120;
+	this.pbpm = this.bpm
+
+	this.starttime = nx.starttime;
+
 	this.init();
 	
 }
@@ -3442,7 +3470,8 @@ util.inherits(matrix, widget);
 
 
 matrix.prototype.init = function() {
-	
+
+
 	this.pmatrix = this.matrix ? this.matrix : false;
 
 	this.matrix = null;
@@ -3454,7 +3483,6 @@ matrix.prototype.init = function() {
 			this.matrix[i][j] = this.pmatrix ? this.pmatrix[i] ? this.pmatrix[i][j] : 0 : 0; // set value of each matrix cell
 		}
 	}
-
 
 	this.draw();
 	
@@ -3612,7 +3640,13 @@ matrix.prototype.sequence = function(bpm) {
 	}	
 	this.sequencing = true;
 	requestAnimationFrame(this.seqStep.bind(this));
- 
+
+}
+
+matrix.prototype.setBPM = function(bpm) {
+	this.bpm = bpm
+	//console.log(bpm)
+	//nx.interval.bpm(this.pulse,bpm)
 }
 
 /** @method stop
@@ -3628,12 +3662,43 @@ matrix.prototype.stop = function() {
 
 matrix.prototype.seqStep = function() {
 
-    var now = new Date().getTime();
-    var dt = now - nx.starttime;
+	if (this.bpm == 0) { this.bpm = 1 }
 
-    this.thisframe = ~~(dt/(60000/this.bpm));
+	//current time
+	var now = new Date().getTime();
+
+	//delta time since start
+	var dt = now - this.starttime;
+
+	if (this.bpm != this.pbpm) {
+
+		//frame + decimal since last beat, in old bpm
+		var timeP = (dt/(60000/this.pbpm))
+
+		// scale to new bpm
+		dt = timeP * (60000/this.bpm)
+
+		//adjust the starttime reference point
+		this.starttime = now - dt
+
+		//calculate new frame #
+		this.thisframe = ~~(dt/(60000/this.bpm));
+
+	} else {
+
+	    //this.thisframe is a constantly ascending integer counter
+	    //to compare with this.lastframe to determine when to increment this.place
+	    //this.thisframe IS NOT the current column.
+	    //the current column is this.place, which is set conditionally below.
+		this.thisframe = ~~(dt/(60000/this.bpm));
+
+	}
+
+	this.pbpm = this.bpm;
 
     if (this.thisframe != this.lastframe) {
+
+		this.lastbeat = now;
 
 		if (this.sequenceMode=="linear") {
 			this.place++;
@@ -3652,10 +3717,10 @@ matrix.prototype.seqStep = function() {
 
     }
 
-    this.lastframe = this.thisframe;
+	this.lastframe = this.thisframe;
     if (this.sequencing) {
 		requestAnimationFrame(this.seqStep.bind(this));
-	}
+	}  
 }
 
 /** @method jumpToCol
@@ -5116,16 +5181,6 @@ position.prototype.draw = function() {
 		}
 	
 		with (this.context) {
-			// draw the x/y level meters
-		/*	beginPath();
-			strokeStyle = this.colors.accent;
-			lineWidth = 5;
-			moveTo(0,this.height);
-			lineTo(this.val.x*this.width,this.height);
-			moveTo(0,this.height);
-			lineTo(0,math.invert(this.val.y)*this.height);					
-			stroke();
-			closePath(); */
 
 			// draw the touch point
 			beginPath();
@@ -5241,14 +5296,14 @@ position.prototype.animate = function(aniType) {
 position.prototype.aniBounce = function() {
 	if (!this.clicked && this.val.x) {
 		this.val.x += (this.deltaMove.x/2)/this.width;
-		this.val.y += (this.deltaMove.y/2)/this.height;
+		this.val.y -= (this.deltaMove.y/2)/this.height;
 		this.val["state"] = "animated";
 		if (math.bounce(this.val.x, 0, 1, this.deltaMove.x) != this.deltaMove.x) {
 			this.deltaMove.x = math.bounce(this.val.x, 0, 1, this.deltaMove.x);
 			this.val["state"] = "bounce";
 		}
-		if (math.bounce(this.val.y, 0, 1, this.deltaMove.y) != this.deltaMove.y) {
-			this.deltaMove.y = math.bounce(this.val.y, 0, 1, this.deltaMove.y);
+		if (this.val.y >= 1 || this.val.y <= 0) {
+			this.deltaMove.y = math.bounce(this.val.y, 0, 1, this.deltaMove.y) * -1;
 			this.val["state"] = "bounce";
 		}
 		this.transmit(this.val);
@@ -8023,4 +8078,250 @@ module.exports = function extend() {
 };
 
 
-},{}]},{},[1]);
+},{}],48:[function(require,module,exports){
+var WAAClock = require('./lib/WAAClock')
+
+module.exports = WAAClock
+if (typeof window !== 'undefined') window.WAAClock = WAAClock
+
+},{"./lib/WAAClock":49}],49:[function(require,module,exports){
+(function (process){
+var isBrowser = (typeof window !== 'undefined')
+
+if (isBrowser && !AudioContext)
+  throw new Error('This browser doesn\'t seem to support web audio API')
+
+var CLOCK_DEFAULTS = {
+  toleranceLate: 0.10,
+  toleranceEarly: 0.001
+}
+
+// ==================== Event ==================== //
+var Event = function(clock, deadline, func) {
+  this.clock = clock
+  this.func = func
+  this._cleared = false // Flag used to clear an event inside callback
+
+  this.toleranceLate = clock.toleranceLate
+  this.toleranceEarly = clock.toleranceEarly
+  this._latestTime = null
+  this._earliestTime = null
+  this.deadline = null
+  this.repeatTime = null
+
+  this.schedule(deadline)
+}
+
+// Unschedules the event
+Event.prototype.clear = function() {
+  this.clock._removeEvent(this)
+  this._cleared = true
+  return this
+}
+
+// Sets the event to repeat every `time` seconds.
+Event.prototype.repeat = function(time) {
+  if (time === 0)
+    throw new Error('delay cannot be 0')
+  this.repeatTime = time
+  if (!this.clock._hasEvent(this))
+    this.schedule(this.deadline + this.repeatTime)
+  return this
+}
+
+// Sets the time tolerance of the event.
+// The event will be executed in the interval `[deadline - early, deadline + late]`
+// If the clock fails to execute the event in time, the event will be dropped.
+Event.prototype.tolerance = function(values) {
+  if (typeof values.late === 'number')
+    this.toleranceLate = values.late
+  if (typeof values.early === 'number')
+    this.toleranceEarly = values.early
+  this._refreshEarlyLateDates()
+  if (this.clock._hasEvent(this)) {
+    this.clock._removeEvent(this)
+    this.clock._insertEvent(this)
+  }
+  return this
+}
+
+// Returns true if the event is repeated, false otherwise
+Event.prototype.isRepeated = function() { return this.repeatTime !== null }
+
+// Schedules the event to be ran before `deadline`.
+// If the time is within the event tolerance, we handle the event immediately.
+// If the event was already scheduled at a different time, it is rescheduled.
+Event.prototype.schedule = function(deadline) {
+  this._cleared = false
+  this.deadline = deadline
+  this._refreshEarlyLateDates()
+
+  if (this.clock.context.currentTime >= this._earliestTime) {
+    this._execute()
+  
+  } else if (this.clock._hasEvent(this)) {
+    this.clock._removeEvent(this)
+    this.clock._insertEvent(this)
+  
+  } else this.clock._insertEvent(this)
+}
+
+Event.prototype.timeStretch = function(tRef, ratio) {
+  if (this.isRepeated())
+    this.repeatTime = this.repeatTime * ratio
+
+  var deadline = tRef + ratio * (this.deadline - tRef)
+  // If the deadline is too close or past, and the event has a repeat,
+  // we calculate the next repeat possible in the stretched space.
+  if (this.isRepeated()) {
+    while (this.clock.context.currentTime >= deadline - this.toleranceEarly)
+      deadline += this.repeatTime
+  }
+  this.schedule(deadline)
+}
+
+// Executes the event
+Event.prototype._execute = function() {
+  if (this.clock._started === false) return
+  this.clock._removeEvent(this)
+
+  if (this.clock.context.currentTime < this._latestTime)
+    this.func(this)
+  else {
+    if (this.onexpired) this.onexpired(this)
+    console.warn('event expired')
+  }
+  // In the case `schedule` is called inside `func`, we need to avoid
+  // overrwriting with yet another `schedule`.
+  if (!this.clock._hasEvent(this) && this.isRepeated() && !this._cleared)
+    this.schedule(this.deadline + this.repeatTime) 
+}
+
+// Updates cached times
+Event.prototype._refreshEarlyLateDates = function() {
+  this._latestTime = this.deadline + this.toleranceLate
+  this._earliestTime = this.deadline - this.toleranceEarly
+}
+
+// ==================== WAAClock ==================== //
+var WAAClock = module.exports = function(context, opts) {
+  var self = this
+  opts = opts || {}
+  this.tickMethod = opts.tickMethod || 'ScriptProcessorNode'
+  this.toleranceEarly = opts.toleranceEarly || CLOCK_DEFAULTS.toleranceEarly
+  this.toleranceLate = opts.toleranceLate || CLOCK_DEFAULTS.toleranceLate
+  this.context = context
+  this._events = []
+  this._started = false
+}
+
+// ---------- Public API ---------- //
+// Schedules `func` to run after `delay` seconds.
+WAAClock.prototype.setTimeout = function(func, delay) {
+  return this._createEvent(func, this._absTime(delay))
+}
+
+// Schedules `func` to run before `deadline`.
+WAAClock.prototype.callbackAtTime = function(func, deadline) {
+  return this._createEvent(func, deadline)
+}
+
+// Stretches `deadline` and `repeat` of all scheduled `events` by `ratio`, keeping
+// their relative distance to `tRef`. In fact this is equivalent to changing the tempo.
+WAAClock.prototype.timeStretch = function(tRef, events, ratio) {
+  events.forEach(function(event) { event.timeStretch(tRef, ratio) })
+  return events
+}
+
+// Removes all scheduled events and starts the clock 
+WAAClock.prototype.start = function() {
+  if (this._started === false) {
+    var self = this
+    this._started = true
+    this._events = []
+
+    if (this.tickMethod === 'ScriptProcessorNode') {
+      var bufferSize = 256
+      // We have to keep a reference to the node to avoid garbage collection
+      this._clockNode = this.context.createScriptProcessor(bufferSize, 1, 1)
+      this._clockNode.connect(this.context.destination)
+      this._clockNode.onaudioprocess = function () {
+        process.nextTick(function() { self._tick() })
+      }
+    } else if (this.tickMethod === 'manual') null // _tick is called manually
+
+    else throw new Error('invalid tickMethod ' + this.tickMethod)
+  }
+}
+
+// Stops the clock
+WAAClock.prototype.stop = function() {
+  if (this._started === true) {
+    this._started = false
+    this._clockNode.disconnect()
+  }  
+}
+
+// ---------- Private ---------- //
+
+// This function is ran periodically, and at each tick it executes
+// events for which `currentTime` is included in their tolerance interval.
+WAAClock.prototype._tick = function() {
+  var event = this._events.shift()
+
+  while(event && event._earliestTime <= this.context.currentTime) {
+    event._execute()
+    event = this._events.shift()
+  }
+
+  // Put back the last event
+  if(event) this._events.unshift(event)
+}
+
+// Creates an event and insert it to the list
+WAAClock.prototype._createEvent = function(func, deadline) {
+  return new Event(this, deadline, func)
+}
+
+// Inserts an event to the list
+WAAClock.prototype._insertEvent = function(event) {
+  this._events.splice(this._indexByTime(event._earliestTime), 0, event)
+}
+
+// Removes an event from the list
+WAAClock.prototype._removeEvent = function(event) {
+  var ind = this._events.indexOf(event)
+  if (ind !== -1) this._events.splice(ind, 1)
+}
+
+// Returns true if `event` is in queue, false otherwise
+WAAClock.prototype._hasEvent = function(event) {
+ return this._events.indexOf(event) !== -1
+}
+
+// Returns the index of the first event whose deadline is >= to `deadline`
+WAAClock.prototype._indexByTime = function(deadline) {
+  // performs a binary search
+  var low = 0
+    , high = this._events.length
+    , mid
+  while (low < high) {
+    mid = Math.floor((low + high) / 2)
+    if (this._events[mid]._earliestTime < deadline)
+      low = mid + 1
+    else high = mid
+  }
+  return low
+}
+
+// Converts from relative time to absolute time
+WAAClock.prototype._absTime = function(relTime) {
+  return relTime + this.context.currentTime
+}
+
+// Converts from absolute time to relative time 
+WAAClock.prototype._relTime = function(absTime) {
+  return absTime - this.context.currentTime
+}
+}).call(this,require('_process'))
+},{"_process":44}]},{},[1]);
