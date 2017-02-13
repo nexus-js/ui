1f8add9388e43e19a39c84f27399a7ece0dc7ee9
Ben Taylor <taylorbf@gmail.com>
2017-02-12 21:56:41 -0500

    toggle and slider

--- dist/mt.js
+++ dist/mt.js
@@ -70,6 +70,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
 
 	var Toggle = __webpack_require__(2);
+	var Slider = __webpack_require__(5);
 
 	var MusiciansToolkit = (function () {
 	  function MusiciansToolkit() {
@@ -101,6 +102,21 @@ return /******/ (function(modules) { // webpackBootstrap
 	      })(function (parent) {
 	        return new Toggle(parent);
 	      })
+	    },
+	    Slider: {
+	      value: (function (_Slider) {
+	        var _SliderWrapper = function Slider(_x2) {
+	          return _Slider.apply(this, arguments);
+	        };
+
+	        _SliderWrapper.toString = function () {
+	          return _Slider.toString();
+	        };
+
+	        return _SliderWrapper;
+	      })(function (parent) {
+	        return new Slider(parent);
+	      })
 	    }
 	  });
 
@@ -120,41 +136,90 @@ return /******/ (function(modules) { // webpackBootstrap
 	var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
 
 	var svg = __webpack_require__(3);
+	var ToggleModel = __webpack_require__(4);
 
 	var Toggle = (function () {
 	  function Toggle(parent) {
 	    _classCallCheck(this, Toggle);
 
 	    this.parent = document.getElementById(parent.replace("#", ""));
+	    this._state = new ToggleModel();
 	    this.buildInterface();
 	  }
 
 	  _createClass(Toggle, {
 	    buildInterface: {
 	      value: function buildInterface() {
+	        var _this = this;
+
 	        this.element = svg.create("svg");
-	        //  this.element.setAttribute('width',100);
-	        //  this.element.setAttribute('height',100);
-	        this.element.style.border = "solid 1px black";
+	        this.element.setAttribute("width", 35);
+	        this.element.setAttribute("height", 20);
 	        this.parent.appendChild(this.element);
 
-	        var bar = svg.create("rect");
-	        bar.setAttribute("x", 10);
-	        bar.setAttribute("y", 10);
-	        bar.setAttribute("rx", 5);
-	        bar.setAttribute("ry", 5);
-	        bar.setAttribute("width", 30);
-	        bar.setAttribute("height", 10);
-	        bar.setAttribute("fill", "#eee");
+	        this.bar = svg.create("rect");
+	        this.bar.setAttribute("x", 0);
+	        this.bar.setAttribute("y", 5);
+	        this.bar.setAttribute("rx", 5);
+	        this.bar.setAttribute("ry", 5);
+	        this.bar.setAttribute("width", 35);
+	        this.bar.setAttribute("height", 10);
+	        this.bar.setAttribute("fill", "#eee");
 
-	        var knob = svg.create("circle");
-	        knob.setAttribute("cx", 10);
-	        knob.setAttribute("cy", 15);
-	        knob.setAttribute("r", 10);
-	        knob.setAttribute("fill", "#d18");
+	        this.knob = svg.create("circle");
+	        this.knob.setAttribute("cx", 10);
+	        this.knob.setAttribute("cy", 10);
+	        this.knob.setAttribute("r", 10);
+	        this.knob.setAttribute("fill", "#d18");
+	        //this.knob.setAttribute('stroke', '#fff');
+	        //this.knob.setAttribute('stroke-width', '3');
 
-	        this.element.appendChild(bar);
-	        this.element.appendChild(knob);
+	        this.element.appendChild(this.bar);
+	        this.element.appendChild(this.knob);
+
+	        this.element.addEventListener("mousedown", function () {
+	          _this.flip();
+	          _this.render();
+	        });
+	      }
+	    },
+	    render: {
+	      value: function render() {
+	        if (this.state) {
+	          this.knob.setAttribute("cx", 10);
+	          this.bar.setAttribute("fill", "#eee");
+	        } else {
+	          this.knob.setAttribute("cx", 25);
+	          this.bar.setAttribute("fill", "#d18");
+	        }
+	      }
+	    },
+	    state: {
+	      get: function () {
+	        return this._state.state;
+	      },
+	      set: function (value) {
+	        var newvalue = this._state.flip(value);
+	        this.render();
+	        return newvalue;
+	      }
+	    },
+	    flip: {
+	      value: function flip() {
+	        this._state.flip();
+	        this.render();
+	      }
+	    },
+	    on: {
+	      value: function on() {
+	        this._state.on();
+	        this.render();
+	      }
+	    },
+	    off: {
+	      value: function off() {
+	        this._state.off();
+	        this.render();
 	      }
 	    }
 	  });
@@ -177,6 +242,162 @@ return /******/ (function(modules) { // webpackBootstrap
 	  }
 
 	};
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	var _createClass = (function () { function defineProperties(target, props) { for (var key in props) { var prop = props[key]; prop.configurable = true; if (prop.value) prop.writable = true; } Object.defineProperties(target, props); } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+	var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+	var Toggle = (function () {
+	  function Toggle() {
+	    _classCallCheck(this, Toggle);
+
+	    this.state = true;
+	  }
+
+	  _createClass(Toggle, {
+	    flip: {
+	      value: function flip(state) {
+	        if (state || state === false) {
+	          this.state = state;
+	        } else {
+	          this.state = !this.state;
+	        }
+	      }
+	    },
+	    on: {
+	      value: function on() {
+	        this.state = true;
+	      }
+	    },
+	    off: {
+	      value: function off() {
+	        this.state = false;
+	      }
+	    }
+	  });
+
+	  return Toggle;
+	})();
+
+	module.exports = Toggle;
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	var _createClass = (function () { function defineProperties(target, props) { for (var key in props) { var prop = props[key]; prop.configurable = true; if (prop.value) prop.writable = true; } Object.defineProperties(target, props); } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+	var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+	var svg = __webpack_require__(3);
+
+	var Slider = (function () {
+	  function Slider(parent) {
+	    _classCallCheck(this, Slider);
+
+	    this.parent = document.getElementById(parent.replace("#", ""));
+	    this.width = 30;
+	    this.height = 100;
+	    this.value = 0.7;
+	    this.buildInterface();
+	  }
+
+	  _createClass(Slider, {
+	    buildInterface: {
+	      value: function buildInterface() {
+	        var _this = this;
+
+	        this.element = svg.create("svg");
+	        this.element.setAttribute("width", this.width);
+	        this.element.setAttribute("height", this.height);
+	        this.parent.appendChild(this.element);
+
+	        var thickness = this.width / 5;
+	        var x1 = this.width / 2 - thickness / 2;
+	        var y1 = 0;
+	        var w = thickness;
+	        var h = this.height;
+	        var knoby = h - this.value * h;
+	        var knobR = thickness * 0.9;
+
+	        this.bar = svg.create("rect");
+	        this.bar.setAttribute("x", x1);
+	        this.bar.setAttribute("y", y1);
+	        this.bar.setAttribute("rx", w / 2);
+	        this.bar.setAttribute("ry", w / 2);
+	        this.bar.setAttribute("width", w);
+	        this.bar.setAttribute("height", h);
+	        this.bar.setAttribute("fill", "#eee");
+
+	        this.fillbar = svg.create("rect");
+	        this.fillbar.setAttribute("x", x1);
+	        this.fillbar.setAttribute("y", knoby);
+	        this.fillbar.setAttribute("rx", w / 2);
+	        this.fillbar.setAttribute("ry", w / 2);
+	        this.fillbar.setAttribute("width", w);
+	        this.fillbar.setAttribute("height", h - knoby);
+	        this.fillbar.setAttribute("fill", "#d18");
+
+	        this.knob = svg.create("circle");
+	        this.knob.setAttribute("cx", this.width / 2);
+	        this.knob.setAttribute("cy", knoby);
+	        this.knob.setAttribute("r", knobR);
+	        this.knob.setAttribute("fill", "#d18");
+
+	        this.element.appendChild(this.bar);
+	        this.element.appendChild(this.fillbar);
+	        this.element.appendChild(this.knob);
+
+	        this.element.addEventListener("mousedown", function (e) {
+	          _this.value = e.pageY / _this.height;
+	          _this.clicked = true;
+	          _this.render();
+	        });
+
+	        this.element.addEventListener("mousemove", function (e) {
+	          _this.value = e.pageY / _this.height;
+	          _this.clicked = true;
+	          _this.render();
+	        });
+
+	        this.element.addEventListener("mouseup", function (e) {
+	          _this.value = e.pageY / _this.height;
+	          _this.clicked = false;
+	          _this.render();
+	        });
+	      }
+	    },
+	    render: {
+	      value: function render() {
+	        this.knob.setAttribute("cy", this.value * this.height);
+	        this.fillbar.setAttribute("y", this.value * this.height);
+	        this.fillbar.setAttribute("height", this.height - this.value * this.height);
+	      }
+
+	      /*  get state() {
+	          return this._state.state;
+	        }
+	        set state(value) {
+	          let newvalue = this._state.flip(value);
+	          this.render();
+	          return newvalue;
+	        } */
+
+	    }
+	  });
+
+	  return Slider;
+	})();
+
+	module.exports = Slider;
 
 /***/ }
 /******/ ])
